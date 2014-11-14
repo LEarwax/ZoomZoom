8dae81e437984c4c2048b9c43f5351d04f81c963
Nick & Amir Fayek <pair+sisypheanpivot.afayek1@devbootcamp.com>
2014-11-14 15:56:06 -0800

    MVP

--- phase 1 project/source/app/controller/controller.rb
+++ phase 1 project/source/app/controller/controller.rb
@@ -1,22 +1,28 @@
 require_relative "../models/edmunds"
 require_relative "../view/view"
 require_relative "../../config/environment"
+require 'pp'
 
 module Controller
   def self.run
-    View.render("Welcome to Zoom Zoom!!!")
-    puts "What do you want to do:"
-    puts "(add)"
-    puts "(list)"
-    command = gets.chomp
+    View.welcome_message
+    View.render("What do you want to do?")
+    View.list(["add", "list","delete"])
+    command = gets.chomp.to_i
     case command
-    when 'add'
-      puts "What year is the car?"
-      year = gets.chomp
-      data = Edmunds.run(year.to_i)
+    when 1
+      View.render("What year is the car?")
+      year = gets.chomp.to_i
+      data = Edmunds.run(year)
       Car.create(data)
-    when 'list'
-      Car.all.each{|car| puts "#{car.year} #{car.make} #{car.model}"}
+    when 2
+      cars = Car.all
+      cars.each { |car| puts "#{car.year} #{car.make} #{car.model} MPG: #{car.mpg_combined} Horsepower: #{car.horsepower} Torque: #{car.torque} Weight: #{car.weight} 0-60: #{car.zero_2_sixty}"}
+    when 3
+      puts "Which car woudl you like to delete?"
+      Car.all.each { |car| puts "#{car.id}. #{car.year} #{car.make} #{car.model}"}
+      car_id = gets.chomp.to_i
+      Car.find(car_id).destroy
     else
       "Sorry that's not a command!"
     end
